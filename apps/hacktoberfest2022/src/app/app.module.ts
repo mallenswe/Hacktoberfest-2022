6a3ef9777627bf16764226a803c2507efd73f0e4
Parastou Safajou <pr2.leclair@gmail.com>
2022-10-13 00:02:46 -0700

    profile-address: Created address component

--- apps/hacktoberfest2022/src/app/app.module.ts
+++ apps/hacktoberfest2022/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { NxWelcomeComponent } from './nx-welcome.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ProfileDetailsComponent } from './components/profile-details/profile-details.component';
 import { ProfileRowComponent } from './components/profile-row/profile-row.component';
+import { AddressComponent } from './components/address/address.component';
 
 @NgModule({
   declarations: [
@@ -14,6 +15,7 @@ import { ProfileRowComponent } from './components/profile-row/profile-row.compon
     NxWelcomeComponent,
     ProfileDetailsComponent,
     ProfileRowComponent,
+    AddressComponent,
   ],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
